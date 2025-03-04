4e5b5ed8e94b8eb0e7f0467bbb634ba58a9f126a
Chris Shewfelt <chris@apromoplace.com>
2025-03-04 19:34:53 +0000

    having trouble interacting with the API

---
+++ src/front/pages/Signup.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react"
+import rigoImageUrl from "../assets/img/rigo-baby.jpg";
+import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+
+export const Signup = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleClick = () => {
+        const opts = {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "email": email,
+                "password": password
+            })
+        }
+    
+        fetch("https://effective-telegram-97jjq94957rxh7j6v-3001.app.github.dev/api/signup", opts)
+        .then(resp => {
+            if(resp.status === 200) return resp.json();
+            else alert("There was an error with the response")
+        })
+        .then(data => {
+            console.log("this came from the backend:", data)
+        })
+        .catch(error => {
+            console.error("There was an error", error)
+        })
+    }
+
+    return (
+        <div className="text-center mt-5">         
+                <form action={handleClick}>
+                    <label for="email">Email:</label>
+                    <input type="text" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)}/><br /><br />
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}/><br /><br />
+                    <input type="submit" value="Login" />
+                </form>
+        </div>
+    );
+}; 
--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -7,6 +7,8 @@ import {
 } from "react-router-dom";
 import { Layout } from "./pages/Layout";
 import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Signup } from "./pages/Signup";
 import { Single } from "./pages/Single";
 import { Demo } from "./pages/Demo";
 
@@ -23,6 +25,8 @@ export const router = createBrowserRouter(
 
         {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
         <Route path= "/" element={<Home />} />
+        <Route path= "/login" element={<Login />} />
+        <Route path= "/signup" element={<Signup />} />
         <Route path="/single/:theId" element={ <Single />} />  {/* Dynamic route for single items */}
         <Route path="/demo" element={<Demo />} />
       </Route>
